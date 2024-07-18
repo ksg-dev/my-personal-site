0463aedc86d6da2c9eaba687832be36c4386ef74
ksg-dev <ksg.dev.data@gmail.com>
2024-07-18 11:04:20 -0500

    add templates dir

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello!"
+    return render_template("sonni.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
